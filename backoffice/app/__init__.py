0ca226c84c02964cdbe4ea7caa02f94ac5795d26
Kaiserr999 <I.camelin74@gmail.com>
2026-07-27 16:58:28 +0200

    added many contents and fixed init

--- backoffice/app/__init__.py
+++ backoffice/app/__init__.py
@@ -3,6 +3,7 @@ from app.config import Config
 from app.extensions import db
 from app.auth.routes import auth_bp
 from app.admin.routes import admin_bp
+from app.stock.routes import stock_bp
 
 def create_app():
     """Creates the app, loads the config and binds it to the DB"""
@@ -11,4 +12,5 @@ def create_app():
     db.init_app(app)
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_bp)
+    app.register_blueprint(stock_bp)
     return app
